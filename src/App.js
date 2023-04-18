81f6b7cb6085170e1e493dcb2c2660d1d7ab2462
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-04-18 11:06:32 -0300

    inserindo imagens de várias formas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import City from "./assets/city.jpg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Seção 3</h1>
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="./img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em assets */}
+      <div>
+        <img src={City} alt="Cidade" />
+      </div>
     </div>
   );
 }
