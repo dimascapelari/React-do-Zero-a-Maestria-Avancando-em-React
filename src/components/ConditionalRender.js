cd3d4264cbf244de97b9d7c9e7edc8f28e729f53
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-04-18 12:03:23 -0300

    if e else no JSX

--- src/components/ConditionalRender.js
+++ src/components/ConditionalRender.js
@@ -3,11 +3,24 @@ import { useState } from "react";
 const ConditionalRender = () => {
   const [x] = useState(false);
 
+  const [name, setName] = useState("Dimas");
+
   return (
     <div>
       <h2>Isso será exibido?</h2>
       {x && <p>Se x for true, sim!</p>}
       {!x && <p>Agora x é falso</p>}
+      <h2>if ternário</h2>
+      {name === "Dimas" ? (
+        <div>
+          <p>O nome é Dimas</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome {name} não foi encontrado</p>
+        </div>
+      )}
+      <button onClick={() => setName("Mariana")}>Clique aqui!</button>
     </div>
   );
 };
